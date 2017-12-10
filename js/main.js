0de770612ff18132eb7422e3c868fdb85259f4e5
AmandaKitSam <amanda_sam@hotmail.co.uk>
2017-12-10 15:09:49 +1100

    Added nav with waypoint feature

--- js/main.js
+++ js/main.js
@@ -3,6 +3,16 @@ console.log("js file connected");
 
 $(document).ready(function() {
 
+  /* For the sticky navigation */
+  $('#features .js-features').waypoint(function(direction) {
+    console.log("enter the area");
+    if (direction == "down") {
+      $('nav').addClass('.sticky');
+    } else {
+      $('nav').removeClass('.sticky');
+    }
+  }); // END.js-features.waypoint
+
 
 
 }); // END.ready function

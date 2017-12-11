aae9c147aa72ef09115fe0de5f0b481b88294100
AmandaKitSam <amanda_sam@hotmail.co.uk>
2017-12-11 18:23:26 +1100

    jQuery for sticky nav working

--- js/main.js
+++ js/main.js
@@ -3,15 +3,29 @@ console.log("js file connected");
 
 $(document).ready(function() {
 
-  /* For the sticky navigation */
-  $('#features .js-features').waypoint(function(direction) {
-    console.log("enter the area");
-    if (direction == "down") {
-      $('nav').addClass('.sticky');
+  // ----- Sticky Navigation -----
+
+  // $('#features .js-features').waypoint(function(direction) {
+  //   if (direction == "down") {
+  //       $('.navbar-fixed-top').addClass('sticky');
+  //   } else {
+  //       $('.navbar-fixed-top').removeClass('sticky');
+  //   }
+  // }, {
+  //   offset: '60px;'
+  // });
+
+  $(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+    // console.log(scroll); // Call the var
+    if (scroll >= 900) {
+      $('.navbar-fixed-top').addClass('sticky');
     } else {
-      $('nav').removeClass('.sticky');
+      $('.navbar-fixed-top').removeClass('sticky');
     }
-  }); // END.js-features.waypoint
+  });
+
+  // ----- END Sticky Navigation -----
 
 
 
